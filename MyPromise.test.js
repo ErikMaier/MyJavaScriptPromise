e1438388df2e03f42946fe20baae6e87a4250866
Erik Maier <erikmaier80@gmail.com>
2022-05-03 20:14:05 +0200

    first commit

---
+++ MyPromise.test.js
@@ -0,0 +1,14 @@
+const MyPromise = require('./MyPromise.js');
+// const MyPromise = Promise
+
+const DEFAULT_VALUE = 'default';
+
+describe('then', () => {});
+
+describe('catch', () => {});
+
+describe('finally', () => {});
+
+describe('static methods', () => {});
+
+function promise({ value = DEFAULT_VALUE, fail = false } = {}) {}
